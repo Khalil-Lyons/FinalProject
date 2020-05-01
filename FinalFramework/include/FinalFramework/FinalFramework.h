ca822ae72c6be247bfc27307091eaa1f4a5decc3
Sebastian Meredith <sebastian.meredith@mymail.champlain.edu>
2020-05-01 13:26:15 -0400

    We did it!

--- FinalFramework/include/FinalFramework/FinalFramework.h
+++ FinalFramework/include/FinalFramework/FinalFramework.h
@@ -1,3 +1,6 @@
-#pragma once
 #include <iostream>
 using namespace std;
+
+int checkwin();//Check win condition varible
+char square[10] = { 'o','1','2','3','4','5','6','7','8','9' }; //Array that creates the Spaces for the board
+void board();//Board Function
